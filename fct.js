814767205699b6feedf8d5d7145dec19b77540bd
Toma Bogdan <bogdantoma2000@gmail.com>
2022-11-28 19:59:39 +0200

    added the ability to change the grid size via range input method

--- fct.js
+++ fct.js
@@ -1,19 +1,20 @@
-let resetbtn = document.querySelector('#resetgamebtn');
+let resetbtn = document.getElementById('resetgamebtn');
+let gridsizebtn = document.getElementById('submitgs');
+let gridsize = document.getElementById('gridsize');
 
-function getGridSize() {
-    let gridsize = document.getElementById('gridsize');
-    console.log(gridsize);
-    return gridsize;
-}
+gridsizebtn.addEventListener('click', () => {
+createDiv();
+hoverDiv();
+});
 
 function createDiv() {
-    for(i = 0; i < 16; i++) {
-        for(j = 0; j < 16; j++) {
+    for(i = 0; i < gridsize.value; i++) {
+        for(j = 0; j < gridsize.value; j++) {
             var crtdivs = document.createElement('div');
             crtdivs.setAttribute('id', `i${i}j${j}`);
             crtdivs.style.backgroundColor = 'white';
-            crtdivs.style.width = '3.75em';
-            crtdivs.style.height = '3.75em';
+            crtdivs.style.width = `${50/gridsize.value}em`;
+            crtdivs.style.height = `${50/gridsize.value}em`;
             document.getElementById('sketch').appendChild(crtdivs);
         }
     }
@@ -21,8 +22,8 @@ function createDiv() {
 
 
 function hoverDiv() {
-    for(i = 0; i < 16; i++) {
-        for(j = 0; j < 16; j++) {
+    for(i = 0; i < gridsize.value; i++) {
+        for(j = 0; j < gridsize.value; j++) {
             let pixels = document.getElementById(`i${i}j${j}`);
             pixels.addEventListener('mousemove', function changeColor() {
                 pixels.style.backgroundColor = 'red';
@@ -31,15 +32,11 @@ function hoverDiv() {
     }
 }
 
-resetbtn.addEventListener ('click', () => {
-    for(i = 0; i < 16; i++) {
-        for(j = 0; j < 16; j++) {
-            const div = document.getElementById(`i${i}j${j}`);
-            div.style.background = 'white';
+resetbtn.addEventListener('click', function resetDrawing() {
+    for(i = 0; i < gridsize.value; i++) {
+        for(j = 0; j < gridsize.value; j++) {
+            let pixelclear = document.getElementById(`i${i}j${j}`);
+            pixelclear.style.background = 'white';
         }
     }
 });
-
-getGridSize();
-createDiv();
-hoverDiv();
