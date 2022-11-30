a789bd5a96497cee5fc99651ec2dca035909f0a0
Toma Bogdan <bogdantoma2000@gmail.com>
2022-11-30 16:09:22 +0200

    now you can update the grid size

--- fct.js
+++ fct.js
@@ -1,29 +1,64 @@
 let resetbtn = document.getElementById('resetgamebtn');
 let gridsizebtn = document.getElementById('submitgs');
 let gridsize = document.getElementById('gridsize');
+let startbtn = document.getElementById('startbtn');
+let updatedGS = 0;
 
-gridsizebtn.addEventListener('click', () => {
-createDiv();
-hoverDiv();
+updateGridSize();
+function updateGridSize() {
+    //gridsizebtn.addEventListener('click', () => {
+    updatedGS = gridsize.value;
+    //console.log(updatedGS);
+    return updatedGS;
+    }
+//)};
+
+startbtn.addEventListener('click', function startGame() {
+    updateGridSize();
+    console.log(updatedGS);
+    createDiv(updatedGS);
+    hoverDiv(updatedGS);
 });
 
-function createDiv() {
-    for(i = 0; i < gridsize.value; i++) {
-        for(j = 0; j < gridsize.value; j++) {
+/*gridsizebtn.addEventListener('click', function updateGridSize() {
+    let updatedGS = gridsize.value;
+    console.log(updatedGS);
+    let a = () => {
+        for(i = 0; i < updatedGS; i++) {
+            for(j = 0; j < updatedGS; j++) {
+                let pixelclear = document.getElementById(`i${i}j${j}`);
+                pixelclear.style.backgroundColor = 'white';
+            }
+    }
+    createDiv(updatedGS);
+    hoverDiv(updatedGS);
+}
+}
+);
+
+/*gridsizebtn.addEventListener('click', () => {
+createDiv();
+hoverDiv();
+console.log(gridsize.value);
+}); */
+
+function createDiv(e) {
+    for(i = 0; i < e; i++) {
+        for(j = 0; j < e; j++) {
             var crtdivs = document.createElement('div');
             crtdivs.setAttribute('id', `i${i}j${j}`);
             crtdivs.style.backgroundColor = 'white';
-            crtdivs.style.width = `${50/gridsize.value}em`;
-            crtdivs.style.height = `${50/gridsize.value}em`;
+            crtdivs.style.width = `${50/e}em`;
+            crtdivs.style.height = `${50/e}em`;
             document.getElementById('sketch').appendChild(crtdivs);
         }
     }
 }
 
 
-function hoverDiv() {
-    for(i = 0; i < gridsize.value; i++) {
-        for(j = 0; j < gridsize.value; j++) {
+function hoverDiv(e) {
+    for(i = 0; i < e; i++) {
+        for(j = 0; j < e; j++) {
             let pixels = document.getElementById(`i${i}j${j}`);
             pixels.addEventListener('mousemove', function changeColor() {
                 pixels.style.backgroundColor = 'red';
@@ -33,10 +68,12 @@ function hoverDiv() {
 }
 
 resetbtn.addEventListener('click', function resetDrawing() {
-    for(i = 0; i < gridsize.value; i++) {
-        for(j = 0; j < gridsize.value; j++) {
+    console.log('Cleared!');
+    for(i = 0; i < updatedGS; i++) {
+        for(j = 0; j < updatedGS; j++) {
             let pixelclear = document.getElementById(`i${i}j${j}`);
             pixelclear.style.background = 'white';
         }
     }
+    createDiv();
 });
